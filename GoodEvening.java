35667ab44ca83e7a298a2a3a406dea4e01f773e4
Sanchr-sys <70843097+Sanchr-sys@users.noreply.github.com>
2021-03-21 02:22:41 +0530

    Update GoodEvening.java

--- GoodEvening.java
+++ GoodEvening.java
@@ -1,7 +1,6 @@
-public class GoodEvening
+class GoodEvening
 {   
-      public static void main(String[] args)
-      {       
+      public static void main(String[] args) {       
          System.out.println("GoodEvening");   
       }
 } 
